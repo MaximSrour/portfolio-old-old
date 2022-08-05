83ec784480cf08c77fd27a990c5fa16a74aa9491
Maxim Srour <maxim.srour@gmail.com>
2022-08-05 21:17:22 +1000

    Minor changes

--- js/icons.js
+++ js/icons.js
@@ -3,7 +3,7 @@ function LoadIcons() {
         let img = el.querySelector('img');
         let text = getComputedStyle(img).getPropertyValue("--iconText");
         text = text.substring(2, text.length-1)
-        
+
         el.innerHTML += text;
     });
 }

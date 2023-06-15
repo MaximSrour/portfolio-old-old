19aca7d35c1c479e27ed9095ba71a5b6a5d6dec6
Maxim Srour <maxim.srour@gmail.com>
2023-06-16 02:29:51 +1000

    Refactoring

--- apps/UnitcodeIconGenerator/script.js
+++ apps/UnitcodeIconGenerator/script.js
@@ -1,197 +1,232 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    // Constants
-    let imageWidth = 512;
-    let imageHeight = 512;
+class UnitcodeIconGenerator {
+    constructor() {
+        this.imageDimensions = {
+            width: 512,
+            height: 512,
+        };
 
-    let textSize = 300;
+        this.configData = {
+            unitcode: "1045",
+            gradientColorSwatch1: "#000000",
+            gradientColorSwatch2: "#FFFFFF",
+            gradientRotation: 0,
+            textSize: 300,
+            textPaddingHorizontal: 100,
+            textPaddingVertical: 50,
+            fontFamily: "Consolas",
+        };
 
-    let unitcode = "1045";
-    let color1 = "#000000";
-    let color2 = "#FFFFFF";
+        this.text = {
+            dimensions: {
+                size: 300,
+                height: 0,
+                width: 0,
+            },
+            padding: {
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+            },
+        };
 
-    let image;
+        this.htmlRef = {
+            unitcodeField: document.getElementById("unitcodeField"),
+            gradientColorSwatch1: document.getElementById("gradientColorSwatch1"),
+            gradientColorSwatch2: document.getElementById("gradientColorSwatch2"),
+            colorRandomButton: document.getElementById("gradientRanomizerButton"),
+            gradientRotationSlider: document.getElementById("gradientRotationSlider"),
+            textSizeSlider: document.getElementById("textSizeSlider"),
+            textPaddingHorizontalSlider: document.getElementById("textPaddingHorizontalSlider"),
+            textPaddingVerticalSlider: document.getElementById("textPaddingVerticalSlider"),
+            fontFamilyDropdown: document.getElementById("fontFamilyDropdown"),
 
-    let textPaddingHorizontal = 100;
-    let textPaddingVertical = 0;
-    
-    let textPaddingHorizontalThreshold = [0, imageWidth / 2];
-    let textPaddingVerticalThreshold = [0, imageHeight / 2];
+            gradientRotationSliderValue: document.getElementById("gradientRotationSliderValue"),
+            textSizeSliderValue: document.getElementById("textSizeSliderValue"),
+            textPaddingHorizontalSliderValue: document.getElementById("textPaddingHorizontalSliderValue"),
+            textPaddingVerticalSliderValue: document.getElementById("textPaddingVerticalSliderValue"),
 
-    const unitcodeField = document.getElementById("unitcode-field");
-    const colorButton1 = document.getElementById("color-button-1");
-    const colorButton2 = document.getElementById("color-button-2");
-    const colorRandomButton = document.getElementById("color-random-button");
-    const gradientRotationSlider = document.getElementById("gradientRotationSlider");
-    const textSizeSlider = document.getElementById("textSizeSlider");
-    const textPaddingHorizontalSlider = document.getElementById("textPaddingHorizontalSlider");
-    const textPaddingVerticalSlider = document.getElementById("textPaddingVerticalSlider");
-    const fontFamilyDropdown = document.getElementById("fontFamilyDropdown");
+            saveButton: document.getElementById("saveButton"),
 
-    const gradientRotationSliderValue = document.getElementById("gradientRotationSliderValue");
-    const textSizeSliderValue = document.getElementById("textSizeSliderValue");
-    const textPaddingHorizontalSliderValue = document.getElementById("textPaddingHorizontalSliderValue");
-    const textPaddingVerticalSliderValue = document.getElementById("textPaddingVerticalSliderValue");
+            outputImage1: document.getElementById("image1"),
+            outputImage2: document.getElementById("image2"),
+            outputImage3: document.getElementById("image3"),
+            outputImage4: document.getElementById("image4"),
+        };
 
-    
-    const saveButton = document.getElementById("saveButton");
+        this.image;
 
-    // Event Listeners
+        this.tempCanvas = document.createElement("canvas");
+        this.diagonal = Math.sqrt(this.imageDimensions.width * this.imageDimensions.width + this.imageDimensions.height * this.imageDimensions.height);
+        this.tempCanvas.width = this.diagonal;
+        this.tempCanvas.height = this.diagonal;
 
-    unitcodeField.addEventListener("keyup", () => {
-        unitcode = unitcodeField.value;
-        updateImage();
-    });
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = this.imageDimensions.width;
+        this.canvas.height = this.imageDimensions.height;
 
-    colorButton1.addEventListener("input", () => {
-        color1 = colorButton1.value;
-        updateImage();
-    });
+        this.CreateEventListeners();
+        this.GenerateRandomColors();
+        this.CalculateImageDimensions();
+        this.UpdateImage();
+    };
 
-    colorButton2.addEventListener("input", () => {
-        color2 = colorButton2.value;
-        updateImage();
-    });
+    CreateEventListeners() {
+        this.htmlRef.unitcodeField.addEventListener("keyup", () => {
+            this.configData.unitcode = this.htmlRef.unitcodeField.value;
+            this.UpdateImage();
+        });
 
-    colorRandomButton.addEventListener("click", generateRandomColors);
+        this.htmlRef.gradientColorSwatch1.addEventListener("input", () => {
+            this.configData.gradientColorSwatch1 = this.htmlRef.gradientColorSwatch1.value;
+            this.UpdateImage();
+        });
 
-    gradientRotationSlider.addEventListener("input", () => {
-        gradientRotationSliderValue.innerHTML = gradientRotationSlider.value;
-        updateImage()
-    });
+        this.htmlRef.gradientColorSwatch2.addEventListener("input", () => {
+            this.configData.gradientColorSwatch2 = this.htmlRef.gradientColorSwatch2.value;
+            this.UpdateImage();
+        });
 
-    textSizeSlider.addEventListener("input", () => {
-        textSizeSliderValue.innerHTML = textSizeSlider.value;
-        updateImage()
-    });
+        this.htmlRef.colorRandomButton.addEventListener("click", () => {
+            this.GenerateRandomColors();
+            this.UpdateImage();
+        });
 
-    textPaddingHorizontalSlider.addEventListener("input", () => {
-        textPaddingHorizontalSliderValue.innerHTML = textPaddingHorizontalSlider.value;
-        updateImage()
-    });
+        this.htmlRef.gradientRotationSlider.addEventListener("input", () => {
+            this.configData.gradientRotation = Number(this.htmlRef.gradientRotationSlider.value);
+            this.htmlRef.gradientRotationSliderValue.innerHTML = this.configData.gradientRotation;
+            this.UpdateImage();
+        });
 
-    textPaddingVerticalSlider.addEventListener("input", () => {
-        textPaddingVerticalSliderValue.innerHTML = textPaddingVerticalSlider.value;
-        updateImage()
-    });
+        this.htmlRef.textSizeSlider.addEventListener("input", () => {
+            this.configData.textSize = Number(this.htmlRef.textSizeSlider.value);
+            this.htmlRef.textSizeSliderValue.innerHTML = this.configData.textSize;
 
-    fontFamilyDropdown.addEventListener("change", updateImage);
+            this.CalculateImageDimensions();
+            this.UpdateImage();
+        });
 
-    // Functions
+        this.htmlRef.textPaddingHorizontalSlider.addEventListener("input", () => {
+            this.configData.textPaddingHorizontal = Number(this.htmlRef.textPaddingHorizontalSlider.value);
+            this.htmlRef.textPaddingHorizontalSliderValue.innerHTML = this.configData.textPaddingHorizontal;
 
-    function generateRandomColors() {
-        colorButton1.value = generateRandomColor();
-        colorButton2.value = generateRandomColor();
-        color1 = colorButton1.value;
-        color2 = colorButton2.value;
-        updateImage();
-    }
+            this.CalculateImageDimensions();
+            this.UpdateImage();
+        });
 
-    function generateRandomColor() {
+        this.htmlRef.textPaddingVerticalSlider.addEventListener("input", () => {
+            this.configData.textPaddingVertical = Number(this.htmlRef.textPaddingVerticalSlider.value);
+            this.htmlRef.textPaddingVerticalSliderValue.innerHTML = this.configData.textPaddingVertical;
+
+            this.CalculateImageDimensions();
+            this.UpdateImage();
+        });
+
+        this.htmlRef.fontFamilyDropdown.addEventListener("change", () => {
+            this.configData.fontFamily = this.htmlRef.fontFamilyDropdown.value;
+            this.UpdateImage();
+        });
+
+        this.htmlRef.saveButton.addEventListener("click", () => {
+            this.SaveImage();
+        });
+    };
+
+    GenerateRandomColors() {
+        this.htmlRef.gradientColorSwatch1.value = this.GenerateRandomColor();
+        this.htmlRef.gradientColorSwatch2.value = this.GenerateRandomColor();
+        this.configData.gradientColorSwatch1 = this.htmlRef.gradientColorSwatch1.value;
+        this.configData.gradientColorSwatch2 = this.htmlRef.gradientColorSwatch2.value;
+    };
+
+    GenerateRandomColor() {
         let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
         return randomColor;
-    }
+    };
 
-    function updateImage() {
-        textSize = Number(textSizeSlider.value);
-        textPaddingHorizontal = Number(textPaddingHorizontalSlider.value);
-        textPaddingVertical = Number(textPaddingVerticalSlider.value);
+    CalculateImageDimensions() {
+        const ctx = this.tempCanvas.getContext("2d");
+        ctx.resetTransform();
+        ctx.font = `bold ${this.configData.textSize}px ${this.configData.fontFamily}`;
+        const textMeasurements = ctx.measureText("0");
 
-        const TEXT_OFFSET_HORIZONTAL = textSize * 0.5
-        const TEXT_OFFSET_VERTICAL = textSize * 1.04
-        const TEXT_OFFSET = textSize * 0.2
+        this.text.dimensions.height = Math.abs(textMeasurements.actualBoundingBoxAscent - textMeasurements.actualBoundingBoxDescent);
+        this.text.dimensions.width = textMeasurements.width;
 
-        const FONT_FAMILY = fontFamilyDropdown.value
+        this.text.padding.top = this.configData.textPaddingVertical + this.text.dimensions.height;
+        this.text.padding.bottom = this.imageDimensions.height - this.configData.textPaddingVertical;
+        this.text.padding.left = this.configData.textPaddingHorizontal;
+        this.text.padding.right = this.imageDimensions.width - this.configData.textPaddingHorizontal - this.text.dimensions.width;
+    };
 
-        // Create a temporary canvas
-        const tempCanvas = document.createElement("canvas");
-        const diagonal = Math.sqrt(imageWidth * imageWidth + imageHeight * imageHeight);
-        tempCanvas.width = diagonal;
-        tempCanvas.height = diagonal;
-        const tempCtx = tempCanvas.getContext("2d");
+    SetImage() {
+        this.image = this.canvas.toDataURL();
+
+        this.htmlRef.outputImage1.src = this.image;
+        this.htmlRef.outputImage2.src = this.image;
+        this.htmlRef.outputImage3.src = this.image;
+        this.htmlRef.outputImage4.src = this.image;
+    };
+
+    UpdateImage() {
+        const tempCtx = this.tempCanvas.getContext("2d");
+        tempCtx.resetTransform();
         
         // Rotate the context of temporary canvas
-        tempCtx.translate(diagonal / 2, diagonal / 2);
-        tempCtx.rotate(gradientRotationSlider.value * Math.PI / 180 - 45);
-        tempCtx.translate(-diagonal / 2, -diagonal / 2);
+        tempCtx.translate(this.diagonal / 2, this.diagonal / 2);
+        tempCtx.rotate(this.configData.gradientRotation * Math.PI / 180 - 45);
+        tempCtx.translate(-this.diagonal / 2, -this.diagonal / 2);
         
         // Draw gradient on temporary canvas
-        const grd = tempCtx.createLinearGradient(0, 0, diagonal, diagonal);
-        grd.addColorStop(0, color1);
-        grd.addColorStop(1, color2);
+        const grd = tempCtx.createLinearGradient(0, 0, this.diagonal, this.diagonal);
+        grd.addColorStop(0, this.configData.gradientColorSwatch1);
+        grd.addColorStop(1, this.configData.gradientColorSwatch2);
         tempCtx.fillStyle = grd;
-        tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
+        tempCtx.fillRect(0, 0, this.tempCanvas.width, this.tempCanvas.height);
 
         // Create the actual canvas and draw rotated gradient
-        const canvas = document.createElement("canvas");
-        canvas.width = imageWidth;
-        canvas.height = imageHeight;
-        const ctx = canvas.getContext("2d");
+        const ctx = this.canvas.getContext("2d");
         ctx.drawImage(
-            tempCanvas,
-            (diagonal - imageWidth) / 2,
-            (diagonal - imageHeight) / 2,
-            imageWidth,
-            imageHeight,
+            this.tempCanvas,
+            (this.diagonal - this.imageDimensions.width) / 2,
+            (this.diagonal - this.imageDimensions.height) / 2,
+            this.imageDimensions.width,
+            this.imageDimensions.height,
             0,
             0,
-            imageWidth,
-            imageHeight
+            this.imageDimensions.width,
+            this.imageDimensions.height
         );
 
         // Draw text
         ctx.fillStyle = "white";
-        ctx.font = `bold ${textSize}px ${FONT_FAMILY}`;
-        const text = unitcodeField.value;
+        ctx.font = `bold ${this.configData.textSize}px ${this.configData.fontFamily}`;
 
-        const TEXT_MEASURE = ctx.measureText(text[0])
+        if (this.configData.unitcode.length > 0) ctx.fillText(this.configData.unitcode[0], this.text.padding.left, this.text.padding.top);
+        if (this.configData.unitcode.length > 1) ctx.fillText(this.configData.unitcode[1], this.text.padding.right, this.text.padding.top);
+        if (this.configData.unitcode.length > 2) ctx.fillText(this.configData.unitcode[2], this.text.padding.left, this.text.padding.bottom);
+        if (this.configData.unitcode.length > 3) ctx.fillText(this.configData.unitcode[3], this.text.padding.right, this.text.padding.bottom);
 
-        const TEXT_HEIGHT = Math.abs(TEXT_MEASURE.actualBoundingBoxAscent - TEXT_MEASURE.actualBoundingBoxDescent)
-        const TEXT_WIDTH = TEXT_MEASURE.width
+        this.SetImage();
+    };
 
-        const TOP = textPaddingVertical + TEXT_HEIGHT
-        const BOTTOM = imageHeight - textPaddingVertical
-        const LEFT = textPaddingHorizontal
-        const RIGHT = imageWidth - textPaddingHorizontal - TEXT_WIDTH
-
-        if (text.length > 0) ctx.fillText(text[0], LEFT, TOP);
-        if (text.length > 1) ctx.fillText(text[1], RIGHT, TOP);
-        if (text.length > 2) ctx.fillText(text[2], LEFT, BOTTOM);
-        if (text.length > 3) ctx.fillText(text[3], RIGHT, BOTTOM);
-
-        image = canvas.toDataURL()
-
-        // Update the image
-        document.getElementById("image1").src = image;
-        document.getElementById("image2").src = image;
-        document.getElementById("image3").src = image;
-        document.getElementById("image4").src = image;
-    }
-    
-    saveButton.addEventListener("click", saveConfig);
-
-    function saveConfig() {
+    SaveImage() {
         let a = document.createElement("a");
-        a.href = image;
-        a.download = `FIT${unitcode}_Icon.png`;
+        a.href = this.image;
+        a.download = `FIT${this.configData.unitcode}_Icon.png`;
         a.click();
+    };
 
-        let configData = {
-            unitCode: unitcodeField.value,
-            color1: color1,
-            color2: color2,
-            gradientRotation: gradientRotationSlider.value,
-            textSize: textSizeSlider.value,
-            textPaddingHorizontal: textPaddingHorizontalSlider.value,
-            textPaddingVertical: textPaddingVerticalSlider.value,
-        };
-        
-        a.href = URL.createObjectURL(new Blob([JSON.stringify(configData, null, 2)], {
+    SaveConfig() {
+        let a = document.createElement("a");
+        a.href = URL.createObjectURL(new Blob([JSON.stringify(this.configData, null, 2)], {
             type: "text/plain"
         }));
-        a.download = `FIT${unitcode}_IconConfig.txt`;
+        a.download = `FIT${this.configData.unitcode}_IconConfig.txt`;
         a.click();
-    }
+    };
+};
 
-    generateRandomColors();
-    updateImage();
+document.addEventListener("DOMContentLoaded", (event) => {
+    new UnitcodeIconGenerator();
 });
